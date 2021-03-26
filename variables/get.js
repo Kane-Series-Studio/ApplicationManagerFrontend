18edcb7492b69f66ae38f17438f2a3b2e4caa95c
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-03-26 15:57:03 -0400

    replace holder

--- variables/get.js
+++ variables/get.js
@@ -1,5 +1,22 @@
-const cpu_speed = require('../../../Application-Manager/backend/js/handler');
+var _speedconstant = 8.9997e-9;
+var d = new Date();
+var amount = 150000000;
 
-console.log(cpu_speed.loops)
+var estprocessor = 1.7;
+console.log("running loop " + amount + " times.\nEstimated time (for " + estprocessor + "ghz processor) is "
+ + (Math.round(((_speedconstant * amount) / estprocessor) * 100) / 100) + "s");
+for (var i = amount; i > 0; i--) { }
+var newd = new Date();
+var accnewd = Number(String(newd.getSeconds()) + "." + String(newd.getMilliseconds()));
+var accd = Number(String(d.getSeconds()) + "." + String(d.getMilliseconds()));
+var di = accnewd - accd;
+if (d.getMinutes() != newd.getMinutes()) {
+    di = (60 * (newd.getMinutes() - d.getMinutes())) + di
+}
+spd = ((_speedconstant * amount) / di);
+console.log("Time: " + Math.round(di * 1000) / 1000 + "s, \nestimated speed: " + Math.round(spd * 1000) / 1000 + "GHZ");
 
-document.getElementById('loops').innerHTML = `Running 15 Million Loops`;
+
+
+document.getElementById('loops').innerHTML = `Running ${amount} Loops`;
+document.getElementById('espro').innerHTML = `Processor: ${estprocessor} ghx processor`
