2e760aa0eab1d0b58a91647231214bad38e43c45
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-03-26 15:50:34 -0400

    loop thread

--- variables/get.js
+++ variables/get.js
@@ -2,4 +2,4 @@ const cpu_speed = require('../../../Application-Manager/backend/js/handler');
 
 console.log(cpu_speed.loops)
 
-document.getElementById('loops').innerHTML = "150000000";
+document.getElementById('loops').innerHTML = `Running 15 Million Loops`;
