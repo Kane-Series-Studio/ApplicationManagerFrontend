af41ca21addd11f58a2936c228a164fd1a373dc0
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-03-26 15:26:22 -0400

    all page title

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    resizable: false
+    resizable: false,
+    title: "Application Manager"
   });
 
   // and load the index.html of the app.
